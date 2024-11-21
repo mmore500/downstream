e3d656dc1a7fcb42b05c35adb4ec4d20768dd8df
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-21 17:53:38 -0500

    Draft and test assign_storage_site_batched

--- downstream/dstream/steady_algo/__init__.pyi
+++ downstream/dstream/steady_algo/__init__.pyi
@@ -1,4 +1,5 @@
 from ._steady_assign_storage_site import assign_storage_site
+from ._steady_assign_storage_site_batched import assign_storage_site_batched
 from ._steady_get_ingest_capacity import get_ingest_capacity
 from ._steady_has_ingest_capacity import has_ingest_capacity
 from ._steady_lookup_ingest_times import lookup_ingest_times
@@ -7,6 +8,7 @@ from ._steady_lookup_ingest_times_eager import lookup_ingest_times_eager
 
 __all__ = [
     "assign_storage_site",
+    "assign_storage_site_batched",
     "get_ingest_capacity",
     "has_ingest_capacity",
     "lookup_ingest_times_batched",
