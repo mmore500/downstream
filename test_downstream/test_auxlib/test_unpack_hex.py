39139f05292e7802449ea11d7c452ca0c6aa812f
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-12-15 22:22:43 -0500

    added endianness of data to unpack_hex function

--- test_downstream/test_auxlib/test_unpack_hex.py
+++ test_downstream/test_auxlib/test_unpack_hex.py
@@ -1,69 +1,65 @@
+import more_itertools
 import numpy as np
 
 from downstream._auxlib._unpack_hex import unpack_hex
 
 
+def _big_to_little_endian(hex_str: str) -> str:
+    return "".join(
+        reversed(
+            [
+                "".join(chunk)
+                for chunk in more_itertools.chunked(hex_str, 2, strict=True)
+            ]
+        )
+    )
+
+
+def _test_unpack_hex(hex_str: str, num_items: int, expected_output: list):
+    np.testing.assert_array_equal(
+        unpack_hex(hex_str, num_items),
+        np.array(expected_output, dtype=np.uint64),
+    )
+    print(hex_str, _big_to_little_endian(hex_str))
+    np.testing.assert_array_equal(
+        unpack_hex(
+            _big_to_little_endian(hex_str), num_items, byteorder="little"
+        ),
+        np.array(expected_output, dtype=np.uint64),
+    )
+
+
 def test_unpack_hex_valid_input():
-    hex_str = "00FF00FABF00FF00"
-    num_items = 2
-    expected_output = np.array([0x00FF00FA, 0xBF00FF00], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("00FF00FABF00FF00", 2, [0x00FF00FA, 0xBF00FF00])
 
 
 def test_unpack_hex_padded_result():
-    hex_str = "AA55AA55AA"
-    num_items = 1
-    expected_output = np.array([0xAA55AA55AA], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("AA55AA55AA", 1, [0xAA55AA55AA])
 
 
 def test_unpack_hex_single_item():
-    hex_str = "FFFFFFFFFFFFFFFF"
-    num_items = 1
-    expected_output = np.array([0xFFFFFFFFFFFFFFFF], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("FFFFFFFFFFFFFFF0", 1, [0xFFFFFFFFFFFFFFF0])
 
 
 def test_unpack_hex_multiple_items():
-    hex_str = "0123456789ABCDEF0123456789ABCDEF"
-    num_items = 2
-    expected_output = np.array(
-        [0x0123456789ABCDEF, 0x0123456789ABCDEF], dtype=np.uint64
+    _test_unpack_hex(
+        "0123456789ABCDEF0123456789ABCDEF",
+        2,
+        [0x0123456789ABCDEF, 0x0123456789ABCDEF],
     )
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
 
 
 def test_unpack_hex_short_hex_string():
-    hex_str = "FF"
-    num_items = 1
-    expected_output = np.array([0xFF], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("FF", 1, [0xFF])
 
 
 def test_unpack_hex_4bit_items():
-    hex_str = "F0F0"
-    num_items = 4
-    expected_output = np.array([0xF, 0x0, 0xF, 0x0], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("F0F0", 4, [0xF, 0x0, 0xF, 0x0])
 
 
 def test_unpack_hex_1bit_items():
-    hex_str = "F8"
-    num_items = 8
-    expected_output = np.array([1, 1, 1, 1, 1, 0, 0, 0], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("F8", 8, [1, 1, 1, 1, 1, 0, 0, 0])
 
 
 def test_unpack_hex_2bit_items():
-    hex_str = "F8"
-    num_items = 4
-    expected_output = np.array([3, 3, 2, 0], dtype=np.uint64)
-    result = unpack_hex(hex_str, num_items)
-    np.testing.assert_array_equal(result, expected_output)
+    _test_unpack_hex("F8", 4, [0x3, 3, 2, 0])
