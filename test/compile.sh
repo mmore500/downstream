7292a5d9b5d7a354cb772f076c4fb6058c2561b3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-18 16:11:12 -0500

    Shrink virtual chip dimensions

--- test/compile.sh
+++ test/compile.sh
@@ -6,12 +6,12 @@ cd "$(dirname "$0")/.."
 
 echo "CSLC ${CSLC}"
 
-# target a 5x4 region of interest
+# target a 1x1 region of interest
 # Every program using memcpy must use a fabric offset of 4,1, and if compiling
 # for a simulated fabric, must use a fabric dimension of at least
-# width+7,height+1, where width and height are the dimensions of the program.
+# width+7,height+2, where width and height are the dimensions of the program.
 # These additional PEs are used by memcpy to route data on and off the wafer.
 # see https://sdk.cerebras.net/csl/tutorials/gemv-01-complete-program/
-# 9x4 because compiler says
-# RuntimeError: Fabric dimension must be at least 9-by-4
-"${CSLC}" --import-path ./include ./test/layout.csl --fabric-dims=15,15 --fabric-offsets=4,1 --channels=1 --memcpy -o out
+# 8x3 because compiler says
+# RuntimeError: Fabric dimension must be at least 8-by-3
+"${CSLC}" --import-path ./include ./test/layout.csl --fabric-dims=8,3 --fabric-offsets=4,1 --channels=1 --memcpy -o out
