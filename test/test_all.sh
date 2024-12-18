91bbcad0eac4d933b2a609fb452dfeb88fc4f964
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-18 17:38:53 -0500

    Add tilted assignment

--- test/test_all.sh
+++ test/test_all.sh
@@ -6,4 +6,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./test.sh 'steady_algo.assign_storage_site'
 ./test.sh 'stretched_algo.assign_storage_site'
-# ./test.sh 'tilted_algo.assign_storage_site'
+./test.sh 'tilted_algo.assign_storage_site'
