62f1edaeed25beb79b91892766138707ff2143c1
Connor Yang <connoryang2005@gmail.com>
2024-12-25 15:50:47 -0500

    Remove private wrapper

--- include/downstream/dstream/steady_algo/_steady_get_ingest_capacity.hpp
+++ include/downstream/dstream/steady_algo/_steady_get_ingest_capacity.hpp
@@ -10,15 +10,9 @@ namespace downstream {
 namespace dstream {
 namespace steady_algo {
 
-const uint64_t _steady_get_ingest_capacity(const uint64_t S) {
+const uint64_t steady_get_ingest_capacity(const uint64_t S) {
   const bool surface_size_ok = std::has_single_bit(S) && S > 1;
   return surface_size_ok ? S : 0;
-}
-
-const std::optional<uint64_t> steady_get_ingest_capacity(const uint64_t S) {
-  const uint64_t ingest_capacity = _steady_get_ingest_capacity(S);
-  return ingest_capacity == S ? std::nullopt
-                              : std::optional<uint64_t>(ingest_capacity);
 }
 
 }  // namespace steady_algo
--- include/downstream/dstream/steady_algo/_steady_has_ingest_capacity.hpp
+++ include/downstream/dstream/steady_algo/_steady_has_ingest_capacity.hpp
@@ -13,8 +13,8 @@ namespace steady_algo {
 
 const bool steady_has_ingest_capacity(const uint64_t S, const uint64_t T) {
   assert(T >= 0);
-  const std::optional<uint64_t> ingest_capacity = steady_get_ingest_capacity(S);
-  return !ingest_capacity.has_value() || T < ingest_capacity.value();
+  const uint64_t ingest_capacity = steady_get_ingest_capacity(S);
+  return ingest_capacity == S || T < ingest_capacity;
 }
 
 }  // namespace steady_algo
