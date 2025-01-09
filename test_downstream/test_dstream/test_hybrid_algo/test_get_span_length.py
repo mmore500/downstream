81c5a89a592ac1046e499cabad89605edb14a218
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-09 06:15:49 -0500

    Test hybrid_algo _get_span_length

--- test_downstream/test_dstream/test_hybrid_algo/test_get_span_length.py
+++ test_downstream/test_dstream/test_hybrid_algo/test_get_span_length.py
@@ -1,5 +1,50 @@
-from downstream.dstream import hybrid_algo as algo
+from downstream import dstream
+from downstream.dstream import hybrid_algo as algo_class
 
 
-def test_smoke():
-    __ = algo
+def test_singleton():
+    algo = algo_class(
+        0,
+        dstream.steady_algo,
+        1,
+    )
+
+    expected = [*range(10)]
+    assert [algo._get_span_length(T, index=0) for T in range(10)] == expected
+
+
+def test_simple():
+    algo = algo_class(
+        0,
+        dstream.steady_algo,
+        1,
+        dstream.stretched_algo,
+        2,
+    )
+    expected = [*range(10)]
+    assert [
+        algo._get_span_length(T, index=0) for T in range(0, 20, 2)
+    ] == expected
+    assert [
+        algo._get_span_length(T, index=1) for T in range(0, 20, 2)
+    ] == expected
+
+
+def test_complex():
+    algo = algo_class(
+        0,
+        dstream.steady_algo,
+        2,
+        dstream.stretched_algo,
+        3,
+        dstream.steady_algo,
+        4,
+    )
+    expected = [*range(0, 20, 2)]
+    assert [
+        algo._get_span_length(T, index=0) for T in range(0, 40, 4)
+    ] == expected
+    expected = [*range(10)]
+    assert [
+        algo._get_span_length(T, index=1) for T in range(0, 40, 4)
+    ] == expected
