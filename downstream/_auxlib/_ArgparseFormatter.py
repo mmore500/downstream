6fc379db0fe3d478ea6991b5639c6992e07dcbd0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-09 22:57:07 -0500

    fixup! Fixup argparse help formatting, add version info

---
+++ downstream/_auxlib/_ArgparseFormatter.py
@@ -0,0 +1,31 @@
+import argparse
+import re
+import textwrap as _textwrap
+
+
+# adapted from https://stackoverflow.com/a/35925919/17332200
+# and https://stackoverflow.com/a/50700187/17332200
+class ArgparseFormatter(
+    argparse.RawDescriptionHelpFormatter,
+    argparse.ArgumentDefaultsHelpFormatter,
+):
+    def __add_whitespace(self, idx, iWSpace, text):
+        if idx == 0:
+            return text
+        return (" " * iWSpace) + text
+
+    def _split_lines(self, text, width):
+        textRows = text.splitlines()
+        for idx, line in enumerate(textRows):
+            search = re.search("\s*[0-9\-]{0,}\.?\s*", line)
+            if line.strip() == "":
+                textRows[idx] = " "
+            elif search:
+                lWSpace = search.end()
+                lines = [
+                    self.__add_whitespace(i, lWSpace, x)
+                    for i, x in enumerate(_textwrap.wrap(line, width))
+                ]
+                textRows[idx] = lines
+
+        return [item for sublist in textRows for item in sublist]
