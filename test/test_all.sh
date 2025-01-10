b33a3db38bb5593a8c488884bc24349933138c2d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-10 03:00:10 -0500

    Implement, test csl hybrid_0_steady_1_tilted_2_algo

--- test/test_all.sh
+++ test/test_all.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./test.sh 'dstream.hybrid_0_steady_1_stretched_2_algo.assign_storage_site'
+./test.sh 'dstream.hybrid_0_steady_1_tilted_2_algo.assign_storage_site'
 ./test.sh 'dstream.steady_algo.assign_storage_site'
 ./test.sh 'dstream.stretched_algo.assign_storage_site'
 ./test.sh 'dstream.tilted_algo.assign_storage_site'
