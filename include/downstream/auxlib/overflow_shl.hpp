84f83780b8c7c337b24edf04b6aea229b15b24de
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-11 07:14:28 -0500

    fixup! Update ingest capacity checks to fix overflow

---
+++ include/downstream/auxlib/overflow_shl.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef DOWNSTREAM_AUXLIB_OVERFLOW_SHL_HPP
+#define DOWNSTREAM_AUXLIB_OVERFLOW_SHL_HPP
+
+#include <concepts>
+
+namespace downstream {
+namespace auxlib {
+
+/**
+ * Perform bitwise shift-left that may overflow without undefined behavior.
+ *
+ * @exceptsafe no-throw
+ */
+template <std::unsigned_integral UINT>
+inline UINT overflow_shl(const UINT a, const UINT b) {
+  return b < sizeof(UINT) * 8 ? a << b : 0;
+}
+
+}  // namespace auxlib
+}  // namespace downstream
+
+#endif  // DOWNSTREAM_AUXLIB_OVERFLOW_SHL_HPP
