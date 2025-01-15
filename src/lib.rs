9fafc0d4dd1d7b149866b58ae3a2e8d6ea99be24
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-15 13:05:05 -0500

    Strip out example starter code

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
-mod _auxlib;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    _auxlib::bit_length(left);
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod _auxlib;
+pub mod dstream;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use downstream::add;
 
 fn main() {
     println!("Hello, world!");
-    println!("{}", add(2, 2));
 }
