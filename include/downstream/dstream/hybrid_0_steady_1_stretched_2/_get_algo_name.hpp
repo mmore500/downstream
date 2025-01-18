1c9fedbad4fea5223725e8ec3b59902d190aa88f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-18 05:51:34 -0500

    fixup! Add get_algo_name(), refactor main.cpp

---
+++ include/downstream/dstream/hybrid_0_steady_1_stretched_2/_get_algo_name.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef DOWNSTREAM_DSTREAM_HYBRID_0_STEADY_1_STRETCHED_2__GET_ALGO_NAME_HPP
+#define DOWNSTREAM_DSTREAM_HYBRID_0_STEADY_1_STRETCHED_2__GET_ALGO_NAME_HPP
+
+#include <string_view>
+
+namespace downstream {
+namespace dstream_hybrid_0_steady_1_stretched_2 {
+
+/**
+ * Get dstream string identifier for algorithm.
+ */
+constexpr std::string_view get_algo_name() {
+  return "dstream.hybrid_0_steady_1_stretched_2_algo";
+}
+
+}  // namespace dstream_hybrid_0_steady_1_stretched_2
+}  // namespace downstream
+
+#endif  // DOWNSTREAM_DSTREAM_HYBRID_0_STEADY_1_STRETCHED_2__GET_ALGO_NAME_HPP
