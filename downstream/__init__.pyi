a5464e279388cc68a9a0452a286d6071012c5e46
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-02-13 14:26:07 -0500

    fix type hints with the version

--- downstream/__init__.pyi
+++ downstream/__init__.pyi
@@ -1,6 +1,13 @@
 from . import dataframe, dstream, dsurf
 
+__author__: str
+__email__: str
+from ._version import __version__  # noqa: F401
+
 __all__ = [
+    "__author__",
+    "__email__",
+    "__version__",
     "dataframe",
     "dstream",
     "dsurf",
