b427c8867c2e06da0e8fdbc30045696f46ce6859
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-02-18 21:30:02 -0500

    List compressing_algo on dstream all

--- downstream/dstream/__init__.pyi
+++ downstream/dstream/__init__.pyi
@@ -1,4 +1,4 @@
-from . import steady_algo, stretched_algo, tilted_algo
+from . import compressing_algo, steady_algo, stretched_algo, tilted_algo
 from ._hybrid_algo import hybrid_algo
 from ._hybrid_algo_prefab import (
     hybrid_0_steady_1_stretched_2_algo,
@@ -6,6 +6,7 @@ from ._hybrid_algo_prefab import (
 )
 
 __all__ = [
+    "compressing_algo",
     "hybrid_algo",
     "hybrid_0_steady_1_stretched_2_algo",
     "hybrid_0_steady_1_tilted_2_algo",
