cc0c3a2cfd41ce3917f35aad63c1cdef500a1472
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-02-20 13:36:38 -0500

    more tests for 0 ingest

--- test_downstream/test_dsurf/test_Surface.py
+++ test_downstream/test_dsurf/test_Surface.py
@@ -1,6 +1,6 @@
+from copy import deepcopy
 import types
 
-from _pytest.compat import num_mock_patch_args
 import opytional as opyt
 import pytest
 
@@ -26,7 +26,7 @@ def test_Surface(algo: types.ModuleType, S: int) -> None:
 
 @pytest.mark.parametrize("algo", [steady_algo, stretched_algo, tilted_algo])
 @pytest.mark.parametrize("S", [8, 16, 32])
-@pytest.mark.parametrize("step_size", [5, 25, 50])
+@pytest.mark.parametrize("step_size", [1, 5, 25, 50])
 def test_Surface_ingest_many(
     algo: types.ModuleType, S: int, step_size: int
 ) -> None:
@@ -42,8 +42,19 @@ def test_Surface_ingest_many(
     )
     for T in range(num_iterations):
         for _ in range(step_size):
-            single_surface.ingest(-T)
-        multi_surface.ingest_multiple(step_size, lambda _: -T)
+            single_surface.ingest(T)
+        multi_surface.ingest_multiple(step_size, lambda _: T)
         assert multi_surface.T == single_surface.T
         assert [*single_surface] == [*multi_surface]
         assert [*single_surface.enumerate()] == [*multi_surface.enumerate()]
+
+
+@pytest.mark.parametrize("algo", [steady_algo, stretched_algo, tilted_algo])
+@pytest.mark.parametrize("S", [8, 16, 32])
+def test_Surface_ingest_none(algo: types.ModuleType, S: int):
+    surf = Surface(algo, S)
+    for T in range(100):
+        surf.ingest(T)
+        new_surf = Surface(algo, deepcopy(surf._storage))
+        new_surf.ingest_multiple(0, lambda _: None)
+        assert [*new_surf] == [*surf]
