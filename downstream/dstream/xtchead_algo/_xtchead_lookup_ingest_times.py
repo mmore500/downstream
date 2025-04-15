ef0ae009d9288204a867b12349d889a9ecfb37b9
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-15 19:28:24 -0400

    Update xtchead_lookup_ingest_times to match behavior

--- downstream/dstream/xtchead_algo/_xtchead_lookup_ingest_times.py
+++ downstream/dstream/xtchead_algo/_xtchead_lookup_ingest_times.py
@@ -36,17 +36,17 @@ def xtchead_lookup_impl(S: int, T: int) -> typing.Iterable[int]:
     assert S > 1 and S.bit_count() == 1
     assert T >= S  # T < S handled by T = S via xtchead_lookup_ingest_times
 
-    epoch = (T - 1).bit_length() + 1
+    epoch = T.bit_length()
     for x in it.islice(
         compressing_lookup_impl(S, max(epoch, S)),
         epoch,
     ):
-        yield (bool(x) << x) >> 1
+        yield (1 << x) - 1
 
     for k in range(epoch, S):
         # https://oeis.org/A057716
         x = k - S.bit_length() + 1
-        yield x + (x + x.bit_length()).bit_length()
+        yield x + (x + x.bit_length()).bit_length() - 1
 
 
 lookup_ingest_times = xtchead_lookup_ingest_times  # lazy loader workaround
