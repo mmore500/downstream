754fce66c8ef0f7ffa7b6520d297307e426fdd30
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-15 13:44:45 -0400

    Impl, test compressing_lookup_ingest_times_batched

--- downstream/dstream/compressing_algo/__init__.pyi
+++ downstream/dstream/compressing_algo/__init__.pyi
@@ -5,6 +5,9 @@ from ._compressing_assign_storage_site_batched import (
 from ._compressing_get_ingest_capacity import get_ingest_capacity
 from ._compressing_has_ingest_capacity import has_ingest_capacity
 from ._compressing_lookup_ingest_times import lookup_ingest_times
+from ._compressing_lookup_ingest_times_batched import (
+    lookup_ingest_times_batched,
+)
 from ._compressing_lookup_ingest_times_eager import lookup_ingest_times_eager
 
 __all__ = [
@@ -12,6 +15,7 @@ __all__ = [
     "assign_storage_site_batched",
     "get_ingest_capacity",
     "has_ingest_capacity",
+    "lookup_ingest_times_batched",
     "lookup_ingest_times_eager",
     "lookup_ingest_times",
 ]
