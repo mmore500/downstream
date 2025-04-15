7a87fad774c504ccc8ebabc93f59383dc44e476b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-15 17:10:18 -0400

    Impl, test xtchead_lookup_ingest_times_batched

--- downstream/dstream/xtchead_algo/__init__.pyi
+++ downstream/dstream/xtchead_algo/__init__.pyi
@@ -3,6 +3,7 @@ from ._xtchead_get_ingest_capacity import get_ingest_capacity
 from ._xtchead_has_ingest_capacity import has_ingest_capacity
 from ._xtchead_has_ingest_capacity_batched import has_ingest_capacity_batched
 from ._xtchead_lookup_ingest_times import lookup_ingest_times
+from ._xtchead_lookup_ingest_times_batched import lookup_ingest_times_batched
 from ._xtchead_lookup_ingest_times_eager import lookup_ingest_times_eager
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "has_ingest_capacity",
     "has_ingest_capacity_batched",
     "lookup_ingest_times_eager",
+    "lookup_ingest_times_batched",
     "lookup_ingest_times",
 ]
