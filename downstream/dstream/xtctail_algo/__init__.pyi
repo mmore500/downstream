d906cce678b603295006bb2c7c43a49652cd612e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-16 20:35:04 -0400

    Draft, test xtctail_lookup_ingest_times

--- downstream/dstream/xtctail_algo/__init__.pyi
+++ downstream/dstream/xtctail_algo/__init__.pyi
@@ -1,9 +1,11 @@
 from ._xtctail_assign_storage_site import assign_storage_site
 from ._xtctail_get_ingest_capacity import get_ingest_capacity
 from ._xtctail_has_ingest_capacity import has_ingest_capacity
+from ._xtctail_lookup_ingest_times import lookup_ingest_times
 
 __all__ = [
     "assign_storage_site",
     "get_ingest_capacity",
     "has_ingest_capacity",
+    "lookup_ingest_times",
 ]
