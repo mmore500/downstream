fae850a386cdd5072e4988707aecbe88a7978a14
Matthew Andres Moreno <m.more500@gmail.com>
2025-04-19 09:45:01 -0500

    Add hybrid {stretched,tilted}xtc algorithms

--- downstream/dstream/__init__.pyi
+++ downstream/dstream/__init__.pyi
@@ -12,7 +12,9 @@ from . import (
 from ._hybrid_algo import hybrid_algo
 from ._hybrid_algo_prefab import (
     hybrid_0_steady_1_stretched_2_algo,
+    hybrid_0_steady_1_stretchedxtc_2_algo,
     hybrid_0_steady_1_tilted_2_algo,
+    hybrid_0_steady_1_tiltedxtc_2_algo,
 )
 
 __all__ = [
@@ -20,7 +22,9 @@ __all__ = [
     "compressing_algo",
     "hybrid_algo",
     "hybrid_0_steady_1_stretched_2_algo",
+    "hybrid_0_steady_1_stretchedxtc_2_algo",
     "hybrid_0_steady_1_tilted_2_algo",
+    "hybrid_0_steady_1_tiltedxtc_2_algo",
     "steady_algo",
     "stretched_algo",
     "stretchedxtc_algo",
