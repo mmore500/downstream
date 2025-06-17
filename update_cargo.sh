3ff3377fc4bb12362ff4f7f5f170468baa634ad2
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-06-17 19:17:31 -0400

    Workaround Cargo submodule bug

--- update_cargo.sh
+++ update_cargo.sh
@@ -2,5 +2,16 @@
 
 set -euo pipefail
 
+cd "$(dirname "$0")"
+
+rm -rf /tmp/downstream-cargo-update
+mkdir /tmp/downstream-cargo-update
+cp -r * /tmp/downstream-cargo-update
+
+pushd /tmp/downstream-cargo-update
 cargo update
+popd
+
+cp /tmp/downstream-cargo-update/Cargo.toml .
+
 git add Cargo.lock
