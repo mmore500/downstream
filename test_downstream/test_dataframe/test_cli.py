e75ef78ef2c1192281e6f51ad2b578f06483b9ad
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-10-25 22:33:03 -0400

    Add CLI regression test

--- test_downstream/test_dataframe/test_cli.py
+++ test_downstream/test_dataframe/test_cli.py
@@ -115,3 +115,16 @@ def test_cli_explode_lookup_unpacked_uint():
         check=True,
         input=(f"{assets}/unpacked.csv\n" * 4096).encode(),
     )
+
+
+def test_cli_regression91():
+    subprocess.run(
+        [
+            "python3",
+            "-m",
+            "downstream.dataframe.explode_lookup_packed_uint",
+            "/dev/null",
+        ],
+        check=True,
+        input=(f"{assets}/regression91.pqt").encode(),
+    )
