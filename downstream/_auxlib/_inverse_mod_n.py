414beecc618ffb38524eb2ea610171ca0b363532
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-02-16 08:24:51 -0500

    Tweak code style

--- downstream/_auxlib/_inverse_mod_n.py
+++ downstream/_auxlib/_inverse_mod_n.py
@@ -1,12 +1,8 @@
 # adapted from https://github.com/ImperialCollegeLondon/Mathematical-Computing-Demo/blob/219bc0e26ea6f5ee7548009c849959b268f54821/M1C%20(Python)/M1C-Number-Theory/.ipynb_checkpoints/Python%20Number%20Theory%2003%20-%20Extended%20Euclidean%20Algorithm-checkpoint.ipynb
 def _ehcf(a: int, b: int) -> int:
     """Extended euclidean algorithm."""
-    p1 = 1
-    q1 = 0
-    h1 = a
-    p2 = 0
-    q2 = 1
-    h2 = b
+    p1, q1, h1 = 1, 0, a
+    p2, q2, h2 = 0, 1, b
 
     while h2:
         r = h1 // h2
