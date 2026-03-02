eb414ac3147486fb0e3c658b1dc2db74c2c10d97
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-03-02 08:52:35 -0500

    Silence parallel cite nag

--- test/src/execute.sh
+++ test/src/execute.sh
@@ -11,6 +11,7 @@ echo "CS_PYTHON ${CS_PYTHON}" >> /tmp/downstream-csl.log
 outfile="$(mktemp)"
 echo "outfile ${outfile}" >>/tmp/downstream-csl.log
 
+mkdir -p ~/.parallel || :; touch ~/.parallel/will-cite || :;
 parallel --pipe -N 2048 -j1 \
     "${CS_PYTHON}" ./test/${set}/client.py --algo "$1" --out "${outfile}" \
     >> /tmp/downstream-csl.log 2>&1
